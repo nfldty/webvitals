6414b8cf1053fd69e65e966abdc2af758d807cf7
Hamza Hijazi <hamza.hijazi@mail.utoronto.ca>
2025-02-25 15:40:09 -0500

    Work on journey tracking complete

--- frontend/script/tracker.js
+++ frontend/script/tracker.js
@@ -1,7 +1,9 @@
 import { trackMouseData } from "./tracker/mouse-movement.js";
 import { trackTimeSpent } from "./tracker/time-tracker.js";
 import { trackPageTransitions } from "./tracker/url-tracking.js";
+import { trackJourney } from "./tracker/url-tracking.js";
 
 trackMouseData();
 trackTimeSpent();
 trackPageTransitions();
+trackJourney();
