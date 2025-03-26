641bc2079478ca10e592954fff3d714505afaa42
Kimberly <kimberly.michela@mail.utoronto.ca>
2025-03-26 15:50:40 -0400

    delete frontend/ and move the page laod tracker to tracker folder and call in in tracker js

--- src/frontend/widget/src/tracker.js
+++ src/frontend/widget/src/tracker.js
@@ -2,8 +2,10 @@ import { trackMouseData } from "./tracker/mouse-movement.js";
 import { trackTimeSpent } from "./tracker/time-tracker.js";
 import { trackPageTransitions } from "./tracker/url-tracking.js";
 import { trackJourney } from "./tracker/user-journey.js";
+import { pageLoadTracker } from "./tracker/page-load-tracker.js";
 
 trackMouseData();
 trackTimeSpent();
 trackPageTransitions();
 trackJourney();
+pageLoadTracker();
