e3afc08555386b0ea6d6ee0731e2c99908fa72dd
nfldty <naufal.adityo16@gmail.com>
2025-03-31 18:08:17 -0400

    modified axios to send cookie

--- src/webapp_client/dashboard/src/utils/api.js
+++ src/webapp_client/dashboard/src/utils/api.js
@@ -1,7 +1,8 @@
 // utils/api.js
 import axios from 'axios';
 const api = axios.create({
-  baseURL: process.env.REACT_APP_WEBAPP_SERVER_URL// Your backend server's base URL
+  baseURL: process.env.REACT_APP_WEBAPP_SERVER_URL,
+  credentials:true// Your backend server's base URL
 });
 
 export default api;
