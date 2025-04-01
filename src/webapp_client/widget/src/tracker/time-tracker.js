5d9365878c34b068b71d2c92f47e9aaeb740a3c6
nfldty <naufal.adityo16@gmail.com>
2025-04-01 01:12:07 -0400

    fix time tracking

--- src/webapp_client/widget/src/tracker/time-tracker.js
+++ src/webapp_client/widget/src/tracker/time-tracker.js
@@ -3,11 +3,11 @@ import { sendData } from '../api-utility.js';
 function trackTimeSpent() {
     let startTime = Date.now();
     let timeSpent = 0;
-
     // when tab visibility changes
     document.addEventListener("visibilitychange", () => {
         if (document.hidden) {
             timeSpent = Date.now() - startTime;
+            sendData("time_tracker", { "time_spent": timeSpent });
         } else {
             startTime = Date.now();
         }
@@ -16,7 +16,6 @@ function trackTimeSpent() {
     // when user is about to leave the webpage
     window.addEventListener("beforeunload", () => {
         timeSpent = Date.now() - startTime;
-        console.log('sending', timeSpent);
         sendData("time_tracker", { "time_spent": timeSpent });
     });
 }
